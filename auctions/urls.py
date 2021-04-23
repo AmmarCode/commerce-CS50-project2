545e9674b5421547d457e1eeabc4d2943f3a4cf0
AmmarCode <ammar.code01@gmail.com>
2021-04-22 17:04:19 -0700

    Add urls for create_listing, view_category, view_listing, closed_listing, view_watchlist, view_listings_by_category, place_bid, close_listing, watch, unwatch, comment

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,16 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create_listing", views.create_listing, name="create_listing"),
+    path("view_category", views.view_category, name="view_category"),
+    path("<int:listing_id>", views.view_listing, name="view_listing"),
+    path("closed_listings", views.closed_listings, name="closed_listings"),
+    path("view_watchlist", views.view_watchlist, name="view_watchlist"),
+    path("<str:category>", views.listings_by_category, name="listings_by_category"),
+    path("<int:listing_id>/place_bid", views.place_bid, name="place_bid"),
+    path("<int:listing_id>/close_listing", views.close_listing, name="close_listing"),
+    path("<int:listing_id>/watch", views.watch, name="watch"),
+    path("<int:listing_id>/unwatch", views.unwatch, name="unwatch"),
+    path("<int:listing_id>/comment",  views.comment, name="comment"),
 ]
